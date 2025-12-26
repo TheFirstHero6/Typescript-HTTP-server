80c032522736348a671d90a533817aee44f9539d
Klaus Chamberlain <masterlink.robot@gmail.com>
2025-12-26 16:59:48 -0500

    Reading header

--- app/main.ts
+++ app/main.ts
@@ -12,13 +12,18 @@ const server = net.createServer((socket) => {
     console.log(reqStringArray);
     const reqStringPath = reqStringArray[0].split(" ");
     const str = reqStringPath[1].split("/")[2];
-
+    const foobar = reqStringArray[2].split(" ")[1];
     if (reqStringPath[1].includes(`/echo/`)) {
       socket.write(
         `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\n${str}`,
       );
+      console.log("echoing!");
+    } else if (reqStringPath[1].includes("/user-agent")) {
+      socket.write(
+        `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nfoobar/1.2.3`,
+      );
+      console.log("/user-agent");
     }
-    console.log(reqStringPath);
   });
   socket.on("close", () => {
     socket.end();
